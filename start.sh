0a02a9f298be53d1d649ffe790ca0952750faa35
Ahmedelnqyb <85938787+Ahmedelnqyb@users.noreply.github.com>
2022-02-26 23:05:55 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/ahmedelnqyb/vvvv /VCPlayerBot
+  git clone https://github.com/ahmedelnqyb/vvvv /vvvv
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/ahmedelnqyb/vvvv -b $BRANCH /VCPlayerBot
+  git clone https://github.com/ahmedelnqyb/vvvv -b $BRANCH /vvvv
 fi
-cd /VCPlayerBot
+cd /vvvv
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
