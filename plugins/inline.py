4b4795d192cbb6370ca3b50ec6ccd8b48ad12c6c
Ahmedelnqyb <85938787+Ahmedelnqyb@users.noreply.github.com>
2022-02-26 21:22:55 +0200

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -31,8 +31,8 @@ from pyrogram import (
 
 buttons = [
     [
-        InlineKeyboardButton('⚡️Make Own Bot', url='https://github.com/subinps/VCPlayerBot'),
-        InlineKeyboardButton('🧩 Join Here', url='https://t.me/subin_works'),
+        InlineKeyboardButton('⚡️Make Own Bot', url='https://t.me/ahmedelnqyb'),
+        InlineKeyboardButton('🧩 Join Here', url='https://t.me/elnqyb'),
     ]
     ]
 def get_cmd(dur):
